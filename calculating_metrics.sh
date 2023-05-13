d2b151de5a9cb75b002376744ffe4e7afd317100
Sarah Zhao <smxz2@cl.cam.ac.uk>
2023-05-13 01:24:56 +0100

    metrics bash script

--- calculating_metrics.sh
+++ calculating_metrics.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
 # Define preprocessing flag values
-preprocessing_flags=('libnormlogtrans' 'logtranscolumnstd' 'libnormlogtranscolumnstd')
-nblikelihood_flags=('nblikelihoodnoscalelearntheta' 'nblikelihoodnoscalefixedtheta1') #'nblikelihoodlearnscalelearntheta' 'nblikelihoodlearnscalefixedtheta1'
+DATA_FLAG='covid_data'
+MODEL_FLAG='gplvm'
+PREPROC_FLAG='rawcounts' #('libnormlogtrans' 'logtranscolumnstd' 'libnormlogtranscolumnstd')
+ENCODER_FLAG=('scalynocovars' 'scaly')
+KERNEL_FLAG='linear_linear'
+LIKELIHOOD_FLAG=('nblikelihoodlearnscalelearntheta' 'nblikelihoodlearnscalefixedtheta1')
+SEED_FLAG='42'
 
 # Activate conda environment
 source activate gplvm
 
 # Run commands
-for flag in "${preprocessing_flags[@]}"
-do
-    python calc_metrics.py --preprocessing "${flag}" --encoder point --likelihood gaussianlikelihood > "./models/covid_data/seed42/gplvm_${flag}_point_gaussianlikelihood_output.txt"
-    python calc_metrics.py --preprocessing "${flag}" --encoder nnenc --likelihood gaussianlikelihood --bio_metrics nmi ari iso_labels_f1 cellASW iso_labels_asw --batch_metrics batchASW graph_connectivity > "./models/covid_data/seed42/gplvm_${flag}_nnenc_gaussianlikelihood_nolisi_output.txt"
-    python calc_metrics.py --preprocessing "${flag}" --encoder scaly --likelihood gaussianlikelihood > "./models/covid_data/seed42/gplvm_${flag}_scaly_gaussianlikelihood_output.txt"
-    python calc_metrics.py --preprocessing "${flag}" --encoder scalynocovars --likelihood gaussianlikelihood > "./models/covid_data/seed42/gplvm_${flag}_scalynocovars_gaussianlikelihood_output.txt"
-done
+# for flag in "${preprocessing_flags[@]}"
+# do
+#     python calc_metrics.py --preprocessing "${flag}" --encoder point --likelihood gaussianlikelihood > "./models/covid_data/seed42/gplvm_${flag}_point_gaussianlikelihood_output.txt"
+#     python calc_metrics.py --preprocessing "${flag}" --encoder nnenc --likelihood gaussianlikelihood --bio_metrics nmi ari iso_labels_f1 cellASW iso_labels_asw --batch_metrics batchASW graph_connectivity > "./models/covid_data/seed42/gplvm_${flag}_nnenc_gaussianlikelihood_nolisi_output.txt"
+#     python calc_metrics.py --preprocessing "${flag}" --encoder scaly --likelihood gaussianlikelihood > "./models/covid_data/seed42/gplvm_${flag}_scaly_gaussianlikelihood_output.txt"
+#     python calc_metrics.py --preprocessing "${flag}" --encoder scalynocovars --likelihood gaussianlikelihood > "./models/covid_data/seed42/gplvm_${flag}_scalynocovars_gaussianlikelihood_output.txt"
+# done
 
-for flag in "${nblikelihood_flags[@]}"
+constant_flags="-m ${MODEL_FLAG} -d ${DATA_FLAG} -k ${KERNEL_FLAG} -s ${SEED_FLAG}"
+
+for l_flag in "${LIKELIHOOD_FLAG[@]}"
 do
-    python calc_metrics.py --preprocessing rawcounts --encoder point --likelihood "${flag}" > "./models/covid_data/seed42/gplvm_rawcounts_point_${flag}_output.txt"
-    python calc_metrics.py --preprocessing rawcounts --encoder scaly --likelihood "${flag}" > "./models/covid_data/seed42/gplvm_rawcounts_scaly_${flag}_output.txt"
-    python calc_metrics.py --preprocessing rawcounts --encoder scalynocovars --likelihood "${flag}" > "./models/covid_data/seed42/gplvm_rawcounts_scalynocovars_${flag}_output.txt"
+    for e_flag in "${ENCODER_FLAG[@]}"
+    do
+        CMD="python calc_metrics.py ${constant_flags} -e ${e_flag} -l ${l_flag} > ./models/${DATA_FLAG}/seed${SEED_FLAG}/${MODEL_FLAG}_${PREPROC_FLAG}_${e_flag}_${KERNEL_FLAG}_${l_flag}_output.txt"
+        echo -e "\nExecuting command:\n==================\n${CMD}\n"
+        eval $CMD
+    done
 done
 
 # Deactivate conda environment
